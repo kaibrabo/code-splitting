420c9ace81bb910d406b6b35017c152f35664fa0
Kai Brabo <kaibrabo@gmail.com>
2018-12-14 16:59:19 -0800

    initial

---
+++ src/components/Page1.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "../logo";
+
+const Page1 = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">Welcome</h1>
+            </header>
+
+            <button onClick={}>Page2</button>
+            <button onClick={}>Page3</button>
+        </div>
+    );
+};
